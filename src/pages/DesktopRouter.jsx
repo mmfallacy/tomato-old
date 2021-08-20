c5e9f3e29f25dee5db820c0182eff2626bc8b411
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-08-21 01:52:08 +0800

    FEAT: Initial Page component (desktop)

--- src/pages/DesktopRouter.jsx
+++ src/pages/DesktopRouter.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
+import Page from "./desktop/Page";
 
 const DesktopPage = () => {
     const { path } = useRouteMatch();
     return (
         <Switch>
-            <Route path={path}>Desktop Home</Route>
+            <Route path={path}>
+                <Page />
+            </Route>
         </Switch>
     );
 };
