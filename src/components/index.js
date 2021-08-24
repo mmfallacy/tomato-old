46fb9225fcd97fe2c8000a719253b5c1ad932487
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-08-24 18:15:13 +0800

    CHORE: Component reexports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
 export { default as Blob } from "./Blob";
 export { default as Page } from "./Page";
+export { default as Text } from "./Text";
+
+export * from "./Navbar";
+export * from "./Background";
+export * from "./Text";
+export * from "./Task";
