84da6b3b3cf7b510129a19d3c6129f1232a29b3f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-08-29 01:18:11 +0800

    FEAT: Text component semibold prop implementation

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 const Text = styled.p`
     font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-    font-weight: ${({ bold }) => bold && "bold"};
+    font-weight: ${({ bold, semibold }) => (bold ? "bold" : semibold && "600")};
     font-size: ${({ size }) => size};
     color: ${({ color }) => color}};
 
@@ -12,11 +12,13 @@ const Text = styled.p`
 
 Text.defaultProps = {
     bold: false,
+    semibold: false,
     size: "1rem",
     color: "#FF8080",
 };
 Text.propTypes = {
     bold: PropTypes.bool,
+    semibold: PropTypes.bool,
     size: PropTypes.string,
     color: PropTypes.string,
 };
