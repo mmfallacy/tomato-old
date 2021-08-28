bd86d56138068e0ef9f2a59fea472fab40b1106e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-08-29 01:59:09 +0800

    STYLE: Task component styling

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,56 @@
 import React from "react";
 import styled from "styled-components";
+import { Text } from "@/components";
 
 const TaskContainer = styled.div`
     display: flex;
     position: relative;
-    background-color: #ffcdcd;
+    background-color: #ff8282;
+    max-height: 48px;
+    width: 100%;
+    padding: 14px;
+    padding-left: 40px;
+    padding-right: 24px;
+    border-radius: 24px;
+    position: relative;
+
+    ${Text} {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    &:before {
+        content: " ";
+        background-color: white;
+        margin: 4px 0;
+        height: 12px;
+        width: 12px;
+        border-radius: 300px;
+        position: absolute;
+        left: 16px;
+    }
+
+    &:hover {
+        max-height: 200px;
+        ${Text} {
+            overflow: visible;
+            text-overflow: clip;
+            white-space: normal;
+        }
+    }
 `;
 
 const Task = ({ name }) => {
-    return <TaskContainer>{name}</TaskContainer>;
+    return (
+        <TaskContainer>
+            <Text semibold size="16px" color="white">
+                {name}
+            </Text>
+        </TaskContainer>
+    );
 };
 
 export default Task;
