b0624fe5879c541c2139c9a0b2616135142df782
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-09-02 18:45:06 +0800

    REFACTOR: Extract Title as a separate component due to possible reuse

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import { Text } from "@/components";
+
+const Title = styled(Text)`
+    color: #ff8080;
+`;
+
+export default Title;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Blob } from "./Blob";
 export { default as Page } from "./Page";
 export { default as Text } from "./Text";
+export { default as Title } from "./Title";
 
 export * from "./Navbar";
 export * from "./Background";
