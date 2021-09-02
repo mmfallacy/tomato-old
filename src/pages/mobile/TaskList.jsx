60b24ff3b9c25e68ae042c5ea0e1f5bd88f0e566
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-09-02 18:41:15 +0800

    FEAT: TaskList page initial implementation

--- src/pages/mobile/TaskList.jsx
+++ src/pages/mobile/TaskList.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import styled from "styled-components";
-import { MobileBG as Background } from "@/components";
-import { MobileNavbar } from "@/components";
-import { Page } from "@/components";
-import { Text } from "@/components";
+import {
+    MobileBG as Background,
+    MobileNavbar,
+    Page,
+    Text,
+    Task,
+} from "@/components";
+import { useCachedState } from "@/hooks";
 
 const PageContainer = styled(Page)`
     padding: 32px;
 `;
 
-const Title = styled(Text)``;
+const Title = styled(Text)`
+    color: #ff8080;
+`;
+
+const TaskListContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 70%;
+
+    ${Task} {
+        margin-bottom: 18px;
+    }
+`;
 
 const TaskList = () => {
+    const [tasks, setTasks] = useCachedState("tasks", []);
     return (
         <PageContainer background={Background}>
             <Title bold size="1.5rem">
                 Tomato
             </Title>
-
+            <TaskListContainer>
+                {tasks.map((el, i) => (
+                    <Task key={i} {...el} />
+                ))}
+            </TaskListContainer>
             <MobileNavbar />
         </PageContainer>
     );
