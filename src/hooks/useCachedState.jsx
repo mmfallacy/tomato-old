fe693710bd1082f6b8df9284c591cbf94b160b99
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-09-02 18:23:46 +0800

    FEAT: Add useCachedState custom hook

---
+++ src/hooks/useCachedState.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+/**
+ * Custom React Hook that retrieves cached state if available and updates cache on state change.
+ * @param {string} name
+ * @param {any} defaultValue
+ * @returns {object} {state, setter}
+ */
+const useCachedState = (name, defaultValue) => {
+    const [state, setter] = useState(defaultValue);
+
+    /**
+     * Retrieve cached if available on mount
+     */
+    useEffect(() => {
+        const item = localStorage.getItem(name);
+        console.log(item);
+        if (item) setter(JSON.parse(item));
+        else localStorage.setItem(name, JSON.stringify(defaultValue));
+    }, []);
+
+    /**
+     * On state change, update cache
+     */
+    useEffect(() => {
+        localStorage.setItem(name, JSON.stringify(state));
+        console.log(name, defaultValue);
+    }, [state]);
+
+    return [state, setter];
+};
+
+export default useCachedState;
