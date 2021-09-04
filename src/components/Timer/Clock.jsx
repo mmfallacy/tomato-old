469f1eafc43000668ce62cbb64b393e5b06447dc
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-09-05 01:46:12 +0800

    FEAT: [WIP] Crude implementation of Clock functionality and animation.

--- src/components/Timer/Clock.jsx
+++ src/components/Timer/Clock.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { ClockRing, ClockRingShadow, ClockRingHead } from "./ClockRing";
 
+// TODO: Refactor timer logic to use setTimeout instead of setInterval for more fluid animation and simpler implementation
+// TODO: Lift state to Timer component
+// FIXME: Refactor timer animation code with readability in mind
+
 const ClockContainer = styled.div`
     height: 100%;
     width: 100%;
@@ -9,28 +13,69 @@ const ClockContainer = styled.div`
     font-size: 8px;
 
     ${ClockRing} {
+        --circumference: calc(${2 * Math.PI} * 50% - 2em);
         position: absolute;
         top: 0;
         left: 0;
     }
 
     ${ClockRingShadow} {
+        --circumference: calc(${2 * Math.PI} * 50% - 6em);
         position: absolute;
         top: -2em;
         left: -2em;
     }
 
+    ${ClockRingShadow}, ${ClockRing} {
+        stroke-dasharray: var(--circumference), var(--circumference);
+
+        transition: stroke-dashoffset 0.1s linear;
+        stroke-dashoffset: calc(
+            var(--circumference) *
+                ${({ percent, $fill }) => ($fill ? percent : 1 + percent)}
+        );
+        transform: rotateZ(-90deg)
+            rotateX(${({ $fill }) => ($fill ? 180 : 0)}deg);
+    }
+
     ${ClockRingHead} {
-        transform: rotateZ(0);
+        transition: transform 1.1s linear;
+        ${({ percent }) => `transform: rotateZ(${360 * percent}deg)`}
     }
 `;
 
-const Clock = ({ className }) => {
+const Clock = ({ className, fill = true }) => {
+    const [percent, setPercent] = React.useState(1);
+    const [time, setTime] = React.useState(120);
+
+    const intervalRef = React.useRef(null);
+
+    const onStart = () => {
+        console.log("Start");
+        setTime((t) => {
+            intervalRef.current = setInterval(onInterval, 1000);
+            return 0;
+        });
+    };
+
+    const onInterval = React.useCallback(() => setTime((t) => t + 1), []);
+    const onEnd = () => {
+        console.log("End");
+        clearInterval(intervalRef.current);
+    };
+
+    React.useEffect(() => {
+        setPercent(time / 120);
+        if (time === 120) onEnd();
+        console.log(time);
+    }, [time]);
+
     return (
-        <ClockContainer className={className}>
+        <ClockContainer className={className} percent={percent} $fill={fill}>
             <ClockRing />
             <ClockRingShadow />
             <ClockRingHead />
+            <button onClick={() => onStart()}>Start</button>
         </ClockContainer>
     );
 };
