b81ee8fec1fe01634d1f40d721b1b706d86c4f18
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-09-06 20:30:09 +0800

    FEAT: Clock Text component

---
+++ src/components/Timer/ClockText.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { Text } from "@/components";
+
+// prettier-ignore
+const secondsToMinSec = (s) =>`${String(Math.floor(s / 60)).padStart(2, "0")}:${String(s % 60).padStart(2,"0")}`;
+
+const StyledClockText = styled(Text)`
+    color: #f08080;
+`;
+
+const _ClockText = ({ className, time, rest }) => (
+    <StyledClockText className={className} {...rest} size="48px" semibold>
+        {secondsToMinSec(Math.ceil(time / 1000))}
+    </StyledClockText>
+);
+
+const ClockText = styled(_ClockText)``;
+
+export default ClockText;
