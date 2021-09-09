c240c527a88be39b3217df70d66f3b1f99fb313c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-09-09 23:12:56 +0800

    STYLE: Context Menu styling

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import { Text } from "@/components";
 
-const ContextMenuContainer = styled.div``;
+const StyledOptions = styled.button`
+    border: none;
+    background: none;
+    width: 100%;
 
-const ContextMenu = () => {
-    return <ContextMenuContainer>x</ContextMenuContainer>;
+    color: ${({ primary, minor }) =>
+        primary ? "#f08080" : minor ? "rgba(0,0,0,0.4)" : "rgba(0,0,0,0.8)"};
+`;
+
+const ContextMenuContainer = styled.div`
+    background-color: white;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 32px;
+    box-shadow: 0px -1px 4px rgba(0, 0, 0, 0.1);
+    border-top-left-radius: 32px;
+    border-top-right-radius: 32px;
+
+    ${StyledOptions}:not(:last-child) {
+        margin-bottom: 16px;
+    }
+`;
+
+const ContextMenuOptions = ({ name, ...rest }) => (
+    <StyledOptions {...rest}>
+        <Text size="20px">{name}</Text>
+    </StyledOptions>
+);
+
+const ContextMenu = ({ options = [], ...props }) => {
+    return (
+        <ContextMenuContainer {...props}>
+            {options.map((el, i) => (
+                <ContextMenuOptions key={i} {...el} />
+            ))}
+        </ContextMenuContainer>
+    );
 };
 
 export default styled(ContextMenu)``;
